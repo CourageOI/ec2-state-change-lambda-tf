4582be5fbf65d192fc0cd7acf77462c66540bd40
Osakpolor Courage Ihensekhien <osakpolor.ihensekhien@kuda.com>
2023-07-06 14:12:46 +0100

    modified:   cloudwatch_events/cloudwatch_event.tf

--- cloudwatch_events/cloudwatch_event.tf
+++ cloudwatch_events/cloudwatch_event.tf
@@ -6,12 +6,14 @@ resource "aws_cloudwatch_event_rule" "ec2_state_change_rule" {
   event_pattern = <<EOF
 {
   "source": ["aws.ec2"],
-  "detail-type": ["EC2 Instance State-change Notification"]
+  "detail-type": ["EC2 Instance State-change Notification"],
+  "detail": {
+    "state": ["running", "stopped", "terminated"]
+  }
 }
 EOF
 }
 
-# Add the Lambda function as a target for the CloudWatch Events rule
 resource "aws_cloudwatch_event_target" "ec2_state_change_target" {
   rule      = aws_cloudwatch_event_rule.ec2_state_change_rule.name
   arn       = var.lambda_function_arn
